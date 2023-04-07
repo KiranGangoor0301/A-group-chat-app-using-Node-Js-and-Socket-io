abad35051db47fcfde4576056dd4e6f03050d2aa
Kiran Gangoor <128992542+KiranGangoor0301@users.noreply.github.com>
2023-04-07 16:36:30 +0530

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,43 @@
+const socket = io('http://localhost:8000',{transports: ['websocket']});
+
+const form=document.getElementById('form');
+const messageInp=document.getElementById('messageInp');
+const container=document.querySelector('.container'); 
+const send=document.getElementsByClassName('send'); 
+
+
+const append=(message,position)=>
+{
+const messageElement=document.createElement('div');
+messageElement.innerText=message;
+messageElement.classList.add('message')
+messageElement.classList.add(position);
+container.append(messageElement);
+}
+form.addEventListener('submit',(e)=>
+{
+    e.preventDefault();
+    const message=messageInp.value;
+    append(`You: ${message}`,'right')
+    socket.emit('send',message);
+    messageInp.value=''
+    
+})
+
+const name=prompt("Enter your name to join");
+socket.emit('new-user-joined',name)
+
+socket.on('user joined',name=>
+{
+append(`${name} is joined to chat`,'right')
+})
+
+socket.on('receive',data=>
+{
+    append(`${data.name}:${data.message}`,'left')
+})
+socket.on('left',name=>
+{
+    append(`${name} left the chat`,'left')
+})
+
